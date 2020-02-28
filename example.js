da60bcdc98516fdaf7f58548723d6c1f41e6d058
Burtonia <alexburtondev@gmail.com>
2020-02-27 22:34:07 -0500

    Added example.js file for reference.

---
+++ example.js
@@ -0,0 +1,9 @@
+
+  jQuery(document).ready(function($) {
+    //Dec 25 2025 12:12:12 GMT-0400
+    var example_countdown = new CountdownJS({
+      id: 'example-countdown',
+      deadline: 'Dec 25 2025 0:00:00 UTC+0000',
+      useTemplate: true
+    });
+  });
